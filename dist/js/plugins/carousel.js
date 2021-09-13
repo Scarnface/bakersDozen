f8136f1327d1e4be1171231aebf12524a79b32f0
Scarnface <scarnface@tutanota.com>
2021-09-13 12:35:07 +0100

    Slick and carousel init

---
+++ dist/js/plugins/carousel.js
@@ -0,0 +1,41 @@
+"use strict";
+
+$(document).ready(function () {
+  //Load the slick carousel and apply the required options.
+  $(".productsCarousel").slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: true,
+    dots: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }, {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
+  });
+  $(".testimonialsCarousel").slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    dots: true
+  }); // Switch between the grab and default styling on mouse down/up.
+
+  $(".carousel").mousedown(function () {
+    $(".carousel").css("cursor", "grab");
+  });
+  $(".carousel").mouseup(function () {
+    $(".carousel").css("cursor", "default");
+  });
+});
