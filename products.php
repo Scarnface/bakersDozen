8496e91464d94d2474f4c5694189103ad3cb0266
Scarnface <scarnface@tutanota.com>
2021-09-20 09:11:03 +0100

    Products page added

--- products.php
+++ products.php
@@ -4,8 +4,136 @@
 
 <?php include "./dist/inc/menu.php" ?>
 
-<!-- Body content goes here -->
+<!-- PRODUCTS -->
+<section class="productOverview">
+  <h1 class="productTitle">Our Products</h1>
+    
+  <div class="products">
+    <div class="container">
+      <div class="productImage">
+        <img src="dist/assets/img/Optimized-bread-min.jpg" alt="Savoury Bread" />
+      </div>
+      <div class="productDescription">
+        <h3>Savoury Breads</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut lobre et dole magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+          Lorem ipsum dolor sit amet.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+          erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+          est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing.
+        </p>
+      </div>
+    </div>
+  </div>
 
-<!-- End body content -->
+  <div class="products">
+    <div class="container">
+      <div class="productImage">
+        <img src="dist/assets/img/Optimized-pastries-min.jpg" alt="Pastries" />
+      </div>
+      <div class="productDescription">
+        <h3>Pastries</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut lobre et dole magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+          Lorem ipsum dolor sit amet.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+          erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+          est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing.
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="products">
+    <div class="container">
+      <div class="productImage">
+        <img src="dist/assets/img/Optimized-cakes-min.jpg" alt="Cakes" />
+      </div>
+      <div class="productDescription">
+        <h3>Cakes</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut lobre et dole magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+          Lorem ipsum dolor sit amet.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+          erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+          est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing.
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="products">
+    <div class="container">
+      <div class="productImage">
+        <img src="dist/assets/img/Optimized-birthday-min.jpg" alt="Birthday Cakes" />
+      </div>
+      <div class="productDescription">
+        <h3>Birthday Cakes</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut lobre et dole magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+          Lorem ipsum dolor sit amet.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+          erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+          est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing.
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="products">
+    <div class="container">
+      <div class="productImage">
+        <img src="dist/assets/img/Optimized-wedding-min.jpg" alt="Wedding Cakes" />
+      </div>
+      <div class="productDescription">
+        <h3>Wedding Cakes</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut lobre et dole magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+          Lorem ipsum dolor sit amet.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+          erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+          est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing.
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="products">
+    <div class="container">
+      <div class="productImage">
+        <img src="dist/assets/img/Optimized-sandwiches-min.jpg" alt="Sandwiches" />
+      </div>
+      <div class="productDescription">
+        <h3>Sandwiches</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut lobre et dole magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+          Lorem ipsum dolor sit amet.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+          erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+          est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing.
+        </p>
+      </div>
+    </div>
+  </div>
+
+</section>
 
 <?php include "./dist/inc/footer.php" ?>
